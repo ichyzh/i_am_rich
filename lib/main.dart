8cc2196ec0063557dd4739185bd804de4763a81f
Mac <mac@MacBook-Pro-Mac.local>
2020-03-23 21:03:30 +0200

    add: asset image, app icon

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: Center(
-          child: Text('Hello World'),
+        home: Scaffold(
+          backgroundColor: Colors.cyanAccent[400],
+          appBar: AppBar(
+            backgroundColor: Colors.cyanAccent[900],
+            title: Text('i_am_rich'),
+          ),
+          body: Center(
+            child: Image(
+              image: AssetImage('images/diamond.png'),
+            ),
+          ),
         ),
       ),
     );
